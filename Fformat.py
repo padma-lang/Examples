8bd7167a262c842fb1a23ff6e20195684aa19bcc
padma-lang <pamidip@hotmail.com>
2021-05-30 20:54:24 -0500

    in branch commit

---
+++ Fformat.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 20:50:02 2021
+
+@author: SRI
+"""
+
+x = input('Enter your name: ')
+y = input('Enter your age: ')
+print(f'{x} is {y} years old!!!')
+
+
